d79823dd9aec5b2eeea537e9812e4ca65d53295e
Olli Lappalainen <olli@lamia.fi>
2018-08-27 11:35:42 +0300

    new exercises

--- app/App.jsx
+++ app/App.jsx
@@ -1,35 +1,81 @@
 import React from 'react';
 import './App.less';
-import Hello from './Hello/Hello.jsx';
-import Counter from './Counter/Counter.jsx';
-import Weather from './Weather/Weather.jsx';
-import GitHubRepoList from './GitHubRepoList/GitHubRepoList.jsx';
 
 export default class App extends React.Component {
-  constructor () {
-    super();    
-  }
-  
-  render() {
-    return (
-      <div className="app-main">
-        <div>
-          <h3>Ex 1</h3>
-          <Hello name="Jorma" location="universe" />
-        </div>        
-        <div>
-          <h3>Ex 2</h3>
-          <Counter />
-        </div>          
-        <div>
-          <h3>Ex 3</h3>
-          <Weather />
-        </div>
-        <div>
-          <h3>Ex 4</h3>
-          <GitHubRepoList />
-        </div>
-      </div>
-    );
-  }
+    constructor () {
+        super();
+
+        this.state = {
+            todos: [],
+            description: '',
+            date: '',
+        }
+    }
+
+    handleInputChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    addTodo = (e) => {
+        e.preventDefault();
+        this.setState({
+            date: '',
+            description: '',
+            todos: [...this.state.todos, {date: this.state.date, description: this.state.description}],
+        });
+    }
+
+    deleteTodo = (item) => {
+        const todos = this.state.todos.filter(todo => item !== todo);
+        this.setState({todos: todos});
+    }
+
+    render() {
+        const todos = this.state.todos.map((todo, index) =>
+            <tr key={index} className="todo-output__tbody-row">
+                <td>{todo.date}</td>
+                <td>{todo.description}</td>
+                <td><button onClick={this.deleteTodo.bind(this, todo)}>DELETE</button></td>
+            </tr>
+        );
+
+        return (
+            <div className="app">
+                <div className="header__wrapper">
+                    <h1 className="header__title">TODO</h1>
+                </div>
+                <div className="todo-form__wrapper">
+                    <form className="todo-form">
+                        <h2 className="todo-form__label">Add ticket:</h2>
+                        <div className="todo-form__inputs-wrapper">
+                            <p>Date</p>
+                            <input className="todo-form__date-input js-todo-input" type="date" name="date" value={this.state.date} onChange={this.handleInputChange} />
+                            <p>Description</p>
+                            <input className="todo-form__description-input js-todo-input" type="text" name="description" value={this.state.description} onChange={this.handleInputChange} />
+                            <button className="todo-form__button" onClick={this.addTodo} type="submit">Add</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="todo-output__wrapper">
+                    <table className="todo-output__table">
+                        <thead className="todo-output__thead">
+                            <tr className="todo-output__thead-row">
+                                <th className="todo-output__thead-text">Date</th>
+                                <th className="todo-output__thead-text">Description</th>
+                            </tr>
+                        </thead>
+                        <tbody className="todo-output__tbody">
+                            {todos}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
